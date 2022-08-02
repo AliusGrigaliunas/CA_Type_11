7f1c40bcfe25f61701548a1645813ec37d6e05e7
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-02 12:10:07 +0300

    feat(64): second slides task done

--- TypeScript [63-81]/64 - typescript įvadas ir tipų aprašymas/tipų aprašymas/src/main.ts
+++ TypeScript [63-81]/64 - typescript įvadas ir tipų aprašymas/tipų aprašymas/src/main.ts
@@ -1,5 +1,64 @@
-const numbers: number[] = [1, 2, 3, 4, 5, 6];
+type Student = {
+  name: string,
+  surname: string,
+  course: number,
+  avg: number,
+}
 
-const doubleNumbers = numbers.map((num) => num * 2);
+type StudentFirstCourse = {
+  name: string,
+  surname: string,
+  course: 1,
+  avg: number,
+}
 
-console.log(doubleNumbers);
+type Fullname = string;
+
+const students: Student[] = [{
+  name: 'Valius',
+  surname: 'Koridas',
+  course: 1,
+  avg: 7.2,
+}, {
+  name: 'Virga',
+  surname: 'Maikaitė',
+  course: 2,
+  avg: 6.3,
+}, {
+  name: 'Šurna',
+  surname: 'Mauzytė',
+  course: 1,
+  avg: 8.1,
+}, {
+  name: 'Grybas',
+  surname: 'Beržauskas',
+  course: 1,
+  avg: 8.1,
+}, {
+  name: 'Surtė',
+  surname: 'Koridaitė',
+  course: 3,
+  avg: 9.7,
+}, {
+  name: 'Vazonius',
+  surname: 'Sėkla',
+  course: 4,
+  avg: 5.2,
+}];
+
+console.group('pilnų vardų masyvas');
+const fullnames: Fullname[] = students.map<Fullname>(({ name, surname }) => `${name} ${surname}`);
+console.table(fullnames);
+console.groupEnd();
+
+console.group('pirmo kurso studentai');
+const studentsFirstCourse: StudentFirstCourse[] = students
+  .filter(({ course }) => course === 1) as StudentFirstCourse[];
+console.table(studentsFirstCourse);
+console.groupEnd();
+
+console.group('visų studentų vidurkis');
+const avg: number = students
+  .reduce((prevSum, student) => prevSum + student.avg, 0) / students.length;
+console.table(avg);
+console.groupEnd();
