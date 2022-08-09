ca95d21a468bca182c5f26c7fbe083bbf346021c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-09 11:59:15 +0300

    feat(69): class intro

--- TypeScript [63-81]/69 - oop - klasės ir konstruktorius/public/js/main.js
+++ TypeScript [63-81]/69 - oop - klasės ir konstruktorius/public/js/main.js
@@ -1,10 +1,22 @@
 "use strict";
-const add = (a, b) => a + b;
-let a = 7;
-let b = 8;
-console.log({ a, b });
-console.log({
-    'add(a, b)': add(a, b),
-    'add(3, 10)': add(3, 10),
-});
+class Car {
+    brand;
+    model;
+    year;
+    color;
+    constructor(brand, model, year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+    printInfo() {
+        console.log(this);
+    }
+}
+const car = new Car('Opel', 'Astra', 1999);
+car.color = 'red';
+console.log(car.brand);
+console.log(car.model);
+console.log(car.year);
+car.printInfo();
 //# sourceMappingURL=main.js.map
