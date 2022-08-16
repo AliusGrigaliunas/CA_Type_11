103fd89f64eda9f50014411d9c42bc545cc9e8ae
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-16 08:28:54 +0300

    feat(73): lesson tasks uploaded

--- TypeScript [63-81]/73 - oop - abstrakcija ir polimorfizmas/atsakymai.ts
+++ TypeScript [63-81]/73 - oop - abstrakcija ir polimorfizmas/atsakymai.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-console */
 /* eslint-disable no-inner-declarations */
@@ -45,165 +44,148 @@
     * Užduočių rezultatų pateikimas tike pat svarbus, kiek sprendimas.
 */
 
-console.group('1. Sukurkite klasę tėvinę Person vaikinėms klasėms ir išsaugokite joje bendrą funkcionalumą.');
+// 80min
+console.group('1. Implementuokite figūrų abstrakciją ir polimorfizmą naudojant abstrakčią klasę');
 {
-  interface PersonProps {
-    name: string;
-    surname: string;
+  // ↓↓↓↓ Klasės ↓↓↓↓
+  abstract class Shape2D {
+    public abstract getPerimeter(): number;
+
+    public abstract getArea(): number;
   }
 
-  class Person {
-    protected name: string;
+  class Rectangle extends Shape2D {
+    private height: number;
 
-    protected surname: string;
+    private width: number;
 
-    constructor({ name, surname }: PersonProps) {
-      this.name = name;
-      this.surname = surname;
+    public constructor(width: number, height: number) {
+      super();
+
+      this.width = width;
+      this.height = height;
     }
 
-    get fullname() {
-      return `${this.name} ${this.surname}`;
-    }
+    public getPerimeter = (): number => 2 * this.height + 2 * this.width;
+
+    public getArea = (): number => this.width * this.height;
   }
 
-  class Student extends Person {
-    private marks: number[];
+  class Circle extends Shape2D {
+    private radius: number;
 
-    constructor(personProps: PersonProps) {
-      super(personProps);
-      this.marks = [];
+    public constructor(radius: number) {
+      super();
+
+      this.radius = radius;
     }
 
-    get avg() {
-      return this.marks.length > 0
-        ? this.marks.reduce((s, x) => s + x) / this.marks.length
-        : 0;
-    }
+    public getPerimeter = (): number => 2 * Math.PI * this.radius;
 
-    addMark(mark: number) {
-      this.marks.push(mark);
-    }
+    public getArea = (): number => Math.PI * this.radius ** 2;
   }
 
-  class Lecturer extends Person {
-    private static MIN_SALARY = 1800;
-
-    private static MAX_SALARY = 4400;
-
-    private static GPM_PERC = 0.20;
-
-    private static PSD_PERC = 0.0698;
-
-    private static VSD_PERC = 0.1252;
-
-    private _salary!: number;
-
-    constructor(salary: number, personProps: PersonProps) {
-      super(personProps);
-      this.salary = salary;
-    }
-
-    set salary(val) {
-      if (val < Lecturer.MIN_SALARY || val > Lecturer.MAX_SALARY) {
-        throw new Error(`Lecturer salary must be in range ${Lecturer.MIN_SALARY}-${Lecturer.MAX_SALARY}`);
-      }
-      const valStr = String(val);
-      const numbersAfterFor = valStr.split('.')[1];
-      if (numbersAfterFor !== undefined && numbersAfterFor.length > 2) {
-        throw new Error('Lecturer salary precision must be no more than 2 points');
-      }
-      this._salary = val;
-    }
-
-    get salary() {
-      return this._salary;
-    }
-
-    get salaryNeto() {
-      const gmpTax = this._salary * Lecturer.GPM_PERC;
-      const psdTax = this._salary * Lecturer.PSD_PERC;
-      const vsdTax = this._salary * Lecturer.VSD_PERC;
-      return Math.round(this._salary - gmpTax - psdTax - vsdTax);
-    }
-  }
-
-  const allPeople: Person[] = [];
-
-  // 20min
-  console.group('1.1. Sukurkite klasę Person, kurios objektams būtų galima priskirti vardą ir pavardę. Šios klasės objektams turi susigeneruoti id - unikalus raktas. Taip pat sukurkite get"erį fullname, kuris grąžina vardą ir pavardę atskirtus tarpu. Atspausdinkite kelis šios klasės objektus, ir pademonstruokite get"erio veikimą.');
-  {
-    const person1 = new Person({ name: 'Varguna', surname: 'Špagelytė' });
-    const person2 = new Person({ name: 'Rainis', surname: 'Parkūras' });
-    const people = [person1, person2];
-    // 1.4
-    allPeople.push(...people);
-
-    people.forEach((p) => console.log(p));
-    people.forEach((p) => console.log(p.fullname));
-  }
-  console.groupEnd();
-
-  // 40min
-  console.group('1.2. Sukurkite klasę Student, kuri paveldėtų klasę Person. Be Person klasės paveldimų savybių, klasę Student turi turėti savybę "marks", kurioje bus saugomi studento pažymiai. Konstruktoriaus vykdymo metu, "marks" reikšmei turi būti sukuriamas tuščias masyvas. Student klasėje sukurkite metodą "addMark" kuris leistų įdėti naują pažymį - sveiką skaičių nuo 1 iki 10. Taip pat sukurkite get"erį "avg", kuris skaičiuotų studento vidurkį pagal esamus pažymius. Sukurkite bent 2 Student klasės objektus ir atspausdinkite visus get"erius ir pavaizduokite metodų funkcionalumą konsolėje.');
-  {
-    const stud1 = new Student({ name: 'Kiauraklis', surname: 'Balkonėjus' });
-    const stud2 = new Student({ name: 'Sulinda', surname: 'Gylaitaitė' });
-    const students = [stud1, stud2];
-    // 1.4
-    allPeople.push(...students);
-
-    console.group('Studenčiokai');
-    students.forEach((s) => console.log(s));
-    console.groupEnd();
-
-    console.group('Studentų vidurkiai:');
-    students.forEach(({ fullname, avg }) => console.log({ fullname, avg }));
-    console.groupEnd();
-
-    console.group('Pridedami pažymiai:');
-    const marks1 = [5, 6, 7];
-    console.log(`Pirmam studentui: ${marks1.join(', ')}`);
-    marks1.forEach((mark) => stud1.addMark(mark));
-
-    const marks2 = [5, 9, 10];
-    console.log(`Antram studentui: ${marks2.join(', ')}`);
-    marks2.forEach((mark) => stud2.addMark(mark));
-    console.groupEnd();
-
-    console.group('Studentų vidurkiai po pridėjimo:');
-    students.forEach(({ fullname, avg }) => console.log({ fullname, avg }));
-    console.groupEnd();
-  }
-  console.groupEnd();
-
-  // 50min
-  console.group('1.3. Sukurkite klasę Lecturer, kuri paveldėtų klasę Person. Papildomai klasei Lecturer sukurkite savybę "salary", kuri privalo būti priskirta objekto sukūrimo metu. Inkapsuliuokite savybę "salary" taip, kad ji galėtų būti skaičius nuo 1800 iki 4400 su ne daugiau nei 2 skaičiais po kablelio. Taip pat sukurkite get"erį "salaryNeto", kuris atspausdintų suapvalintą atlyginimą iki sveikų skaičių atskaičiavus mokesčius: GPM 20%, PSD 6.98%, VSD 12.52%. Sukurkite bent 2 Lecturer klasės objektus ir atspausdinkite visus get"erius konsolėje.');
-  {
-    const lecturer1 = new Lecturer(3000, { name: 'Servacijus', surname: 'Tritonas' });
-    const lecturer2 = new Lecturer(3200, { name: 'Vorė', surname: 'Tinklaitienė' });
-    const lecturers = [lecturer1, lecturer2];
-    // 1.4
-    allPeople.push(...lecturers);
-
-    console.group('Dėstytuvai:');
-    lecturers.forEach((l) => console.log(l));
-    console.groupEnd();
-
-    console.group('Dėstytojų atlyginimai:');
-    lecturers.forEach(({ fullname, salary, salaryNeto }) => console.log({
-      fullname,
-      salary,
-      salaryNeto,
-    }));
-    console.groupEnd();
-  }
-  console.groupEnd();
+  // ↑↑↑↑ Klasės ↑↑↑↑
 
   // 10min
-  console.group('1.4. Sukurkite viešai [1.] užduočiai pasiekiamą masyvą "allPeople". [1.1], [1.2] ir [1.3] užduotyse sukurtus objektus įdėkite į šį masyvą. Atspausdinkite visų žmonių elementų "fullname"');
+  console.log('1.1. Sukurkite abstrakčią klasę Shape2D, kuri turėtų 2 abstrakčius metodus. Abu šie metodai neturi parametrų ir grąžina skaičių: "getPerimeter" ir "getArea"');
+
+  // 30min
+  console.log('1.2. Sukurkite Rectangle klasę kuri paveldi Shape2D klasę ir implementuokite metodus. Konstruktoriaus ir savybes pasirinkite taip, kad galėtumėte implementuoti abstrakčių metodų logiką.');
+
+  // 30min
+  console.log('1.3. Sukurkite Circle klasę kuri paveldi Shape2D klasę ir implementuokite metodus. Konstruktoriaus ir savybes pasirinkite taip, kad galėtumėte implementuoti abstrakčių metodų logiką.');
+
+  // 10min
+  console.groupCollapsed('1.4. Sukurkite "Shape2D" tipo masyvą, kuriame būtų 2 apskritimai ir 2 keturkampiai. Atspausdinkite visų figūrų plotus ir perimetrus naudodami "Shape2D" klasės abstrakčius metodus');
   {
-    allPeople.forEach(({ fullname }) => console.log(fullname));
+    const shapes: Shape2D[] = [
+      new Rectangle(500, 200),
+      new Rectangle(300, 300),
+      new Circle(40),
+      new Circle(70),
+    ];
+
+    console.log('Figūros:');
+    console.table(shapes);
+
+    console.group('Perimetrai:');
+    shapes.forEach((shape) => {
+      console.log(shape.getPerimeter());
+    });
+    console.groupEnd();
+
+    console.group('Plotai:');
+    shapes.forEach((shape) => {
+      console.log(shape.getPerimeter());
+    });
+    console.groupEnd();
   }
+  console.groupEnd();
+}
+console.groupEnd();
+
+// 20min
+console.group('2. Implementuokite figūrų abstrakciją ir polimorfizmą naudojant interface\'ą');
+{
+  // ↓↓↓↓ Klasės ↓↓↓↓
+  interface Shape2D {
+    getPerimeter(): number;
+    getArea(): number;
+  }
+
+  class Rectangle implements Shape2D {
+    public constructor(private width: number, private height: number) { }
+
+    public getPerimeter = (): number => 2 * this.height + 2 * this.width;
+
+    public getArea = (): number => this.width * this.height;
+  }
+
+  class Circle implements Shape2D {
+    public constructor(private radius: number) { }
+
+    public getPerimeter = (): number => 2 * Math.PI * this.radius;
+
+    public getArea = (): number => Math.PI * this.radius ** 2;
+  }
+
+  // ↑↑↑↑ Klasės ↑↑↑↑
+
+  // 5min
+  console.log('2.1. Sukurkite abstrakčią klasę Shape2D, kuri turėtų 2 abstrakčius metodus. Abu šie metodai neturi parametrų ir grąžina skaičių: "getPerimeter" ir "getArea"');
+
+  // 5min
+  console.log('2.2. Sukurkite Rectangle klasę kuri paveldi Shape2D klasę ir implementuokite metodus. Konstruktoriaus ir savybes pasirinkite taip, kad galėtumėte implementuoti abstrakčių metodų logiką.');
+
+  // 5min
+  console.log('2.3. Sukurkite Circle klasę kuri paveldi Shape2D klasę ir implementuokite metodus. Konstruktoriaus ir savybes pasirinkite taip, kad galėtumėte implementuoti abstrakčių metodų logiką.');
+
+  // 5min
+  console.groupCollapsed('2.4. Sukurkite "Shape2D" tipo masyvą, kuriame būtų 2 apskritimai ir 2 keturkampiai. Atspausdinkite visų figūrų plotus ir perimetrus naudodami "Shape2D" klasės abstrakčius metodus');
+  {
+    const shapes: Shape2D[] = [
+      new Rectangle(500, 200),
+      new Rectangle(300, 300),
+      new Circle(40),
+      new Circle(70),
+    ];
+
+    console.log('Figūros:');
+    console.table(shapes);
+
+    console.group('Perimetrai:');
+    shapes.forEach((shape) => {
+      console.log(shape.getPerimeter());
+    });
+    console.groupEnd();
+
+    console.group('Plotai:');
+    shapes.forEach((shape) => {
+      console.log(shape.getPerimeter());
+    });
+    console.groupEnd();
+  }
+  console.groupEnd();
 }
 console.groupEnd();
