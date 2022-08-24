c2e8aacfdae2efc58207baa4e2dd2945077d9fe7
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-24 10:15:18 +0300

    task(78): implement ProductsCollection.joinProduct method

--- TypeScript [63-81]/79 - CRUD 1/uzduotys/src/helpers/products-collection.ts
+++ TypeScript [63-81]/79 - CRUD 1/uzduotys/src/helpers/products-collection.ts
@@ -1,5 +1,17 @@
-class ProductsCollection {
+import Product from '../types/product';
+import Category from '../types/category';
+import ProductCategory from '../types/product-category';
 
+class ProductsCollection {
+  constructor(
+    private products: Product[],
+    private categories: Category[],
+    private productCategories: ProductCategory[],
+  ) { }
+
+  private joinProduct(product: Product): ProductJoined {
+
+  }
 }
 
 export default ProductsCollection;
