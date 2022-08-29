e6f6a15e4c66180c52973b8688f4cade836817e9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-29 11:36:00 +0300

    feat(82): example component

--- React [82-]/82 - React aplikacijos pagrindai/src/App.tsx
+++ React [82-]/82 - React aplikacijos pagrindai/src/App.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ManoParagrafas from './mano-paragrafas';
+
+// <ManoParagrafas content="Pirmas paragrafas" author="Jalamonis Kirpaila" />
+// ManoParagrafas({
+//   content: "Pirmas paragrafas",
+//   author: "Jalamonis Kirpaila"
+// })
 
 function App() {
+  const title = 'Mano pirma aplikacija'
+  const linkText = 'Išmok React!';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. kkkkkkkkkkkkkkkkkkk
-        </p>
+        <p>{title}</p>
+        <ManoParagrafas content="Pirmas paragrafas" author="Jalamonis Kirpaila" />
+        <ManoParagrafas content="Antras paragrafas" />
+        <ManoParagrafas content="Trečias paragrafas" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {linkText} aaaa
         </a>
       </header>
     </div>
