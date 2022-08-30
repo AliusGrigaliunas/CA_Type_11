cad612877e8d5b4b12d391464a34b20a1f561521
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-30 11:50:03 +0300

    feat(83): sccs mixin and scss function example

--- React [82-]/83 - Props and children/src/App.tsx
+++ React [82-]/83 - Props and children/src/App.tsx
@@ -6,11 +6,13 @@ const App: React.FC = () => {
   return (
     <main>
       <h1>Mano pirmoji aplikacija</h1>
-      <Button>mygtukas</Button>
-      <Button color="primary">primary mygtukas</Button>
-      <Button color="secondary">secondary mygtukas</Button>
-      <Button color="error">error mygtukas</Button>
-      <Button color="warning">warning mygtukas</Button>
+      <div style={{ display: 'flex', gap: 10 }}>
+        <Button>mygtukas</Button>
+        <Button color="primary">primary mygtukas</Button>
+        <Button color="secondary">secondary mygtukas</Button>
+        <Button color="error">error mygtukas</Button>
+        <Button color="warning">warning mygtukas</Button>
+      </div>
     </main>
   );
 }
