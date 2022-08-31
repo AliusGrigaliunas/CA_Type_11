7e23e0a33f744b6b736869e5ec5c8153f81fa7df
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-08-31 08:47:18 +0300

    feat(84): cleared project

--- React [82-]/84 - Page UI using components 1/src/app.tsx
+++ React [82-]/84 - Page UI using components 1/src/app.tsx
@@ -1,18 +1,10 @@
 import * as React from 'react';
-import Button from './components/button';
 
 const App: React.FC = () => {
 
   return (
     <main>
       <h1>Mano pirmoji aplikacija</h1>
-      <div style={{ display: 'flex', gap: 10 }}>
-        <Button>mygtukas</Button>
-        <Button color="primary">primary mygtukas</Button>
-        <Button color="secondary">secondary mygtukas</Button>
-        <Button color="error">error mygtukas</Button>
-        <Button color="warning">warning mygtukas</Button>
-      </div>
     </main>
   );
 }
