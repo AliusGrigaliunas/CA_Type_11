c6a5252cfb3905a4eb9e5d50805ab30ca85156e2
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-01 08:02:56 +0300

    init(85): useState hook

---
+++ React [82-]/85 - useState hook/src/app.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Hero from './components/hero';
+import SideBox from './components/side-box';
+
+const App: React.FC = () => {
+
+  return (
+    <main>
+      <Hero />
+      <SideBox />
+    </main>
+  );
+}
+
+export default App;
+
+
