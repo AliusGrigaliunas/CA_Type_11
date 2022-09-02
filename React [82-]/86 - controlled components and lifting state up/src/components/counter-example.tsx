851e5650edb50d4dc377cd516653001e6bb05851
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-02 10:32:48 +0300

    feat(86): button component created

--- React [82-]/86 - controlled components and lifting state up/src/components/counter-example.tsx
+++ React [82-]/86 - controlled components and lifting state up/src/components/counter-example.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import Input from './input';
-
-const buttonStyle = {
-  fontSize: 40,
-  padding: '10px 20px',
-  cursor: 'pointer',
-};
+import Button from './button';
 
 const CounterExample = () => {
   //       ↙ - kitamasis naudojimui: atvaizdavimui, arba perdavimui į kitus komponenetus
@@ -17,7 +12,6 @@ const CounterExample = () => {
   //                         ↖ - funkcija, skirta keisti reikšmei ir perkrauti komponentui.
   const [step, setStep] = React.useState<number>(3);
 
-  // TODO: BUTTON + children
   return (
     <div style={{ fontSize: 50, textAlign: 'center', marginTop: '2rem' }}>
       <div>{`Count: ${count}`}</div>
@@ -30,22 +24,8 @@ const CounterExample = () => {
         display: 'flex', justifyContent: 'center', gap: 20, marginTop: '1rem',
       }}
       >
-        <button
-          type="button"
-          style={buttonStyle}
-          //                ↙ - kviečiama state keitimo funkcija, perduodant naują reikšmę
-          onClick={() => setCount(count - step)}
-        >
-          Mazinti
-        </button>
-        <button
-          type="button"
-          style={buttonStyle}
-          //                ↙ - kviečiama state keitimo funkcija, perduodant naują reikšmę
-          onClick={() => setCount(count + step)}
-        >
-          Didinti
-        </button>
+        <Button onClick={() => setCount(count - step)}>Mazinti</Button>
+        <Button onClick={() => setCount(count + step)}>Didinti</Button>
       </div>
     </div>
   );
