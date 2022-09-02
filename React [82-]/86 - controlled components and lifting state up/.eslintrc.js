8f4708a673fafc89cb979face9b43187f898eb74
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-02 08:48:50 +0300

    chore(86): updated eslint rules

--- React [82-]/86 - controlled components and lifting state up/.eslintrc.js
+++ React [82-]/86 - controlled components and lifting state up/.eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
         project: './tsconfig.json'
     },
     rules: {
+        'react/button-has-type': [0],
+        'react/prop-types': [0],
+        'react/require-default-props': [0],
         'react/function-component-definition': [2, {
             namedComponents: 'arrow-function',
             unnamedComponents: 'arrow-function',
