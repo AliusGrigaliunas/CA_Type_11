4ead700782211a23a155e12c7ef28e667801cd41
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-05 09:26:35 +0300

    task(87): Button task

--- React [82-]/87 - Mui - basic components, icons and compoennt API/src/app.tsx
+++ React [82-]/87 - Mui - basic components, icons and compoennt API/src/app.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import Container from './components/container';
-import CounterExample from './components/counter-example';
+import {
+  Typography,
+  Container,
+} from '@mui/material';
 
 const App: React.FC = () => (
   <Container>
-    <CounterExample />
+    <Typography variant="h2" component="h1" align="center">Tai yra mano aplikacija</Typography>
+    {/*
+      1. Tuščiaviduris didelis mygtukas
+      2. Pilnaviduris mažas mygtukas
+      3. Raudonas mygtuka su tekstu ir  ikona gale
+      4. Žalias mygtukas su tekstu ir ikona prekyje
+      5. Geltonas mygtukas TIK su ikona
+    */}
   </Container>
 );
 
