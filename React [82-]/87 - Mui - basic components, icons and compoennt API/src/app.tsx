f71daac788c7d8a09f1cb37aa40501651c68847a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-05 09:52:22 +0300

    task-done(87): Button task

--- React [82-]/87 - Mui - basic components, icons and compoennt API/src/app.tsx
+++ React [82-]/87 - Mui - basic components, icons and compoennt API/src/app.tsx
@@ -2,18 +2,31 @@ import * as React from 'react';
 import {
   Typography,
   Container,
+  Button,
+  IconButton,
 } from '@mui/material';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
 
 const App: React.FC = () => (
   <Container>
     <Typography variant="h2" component="h1" align="center">Tai yra mano aplikacija</Typography>
-    {/*
-      1. Tuščiaviduris didelis mygtukas
-      2. Pilnaviduris mažas mygtukas
-      3. Raudonas mygtuka su tekstu ir  ikona gale
-      4. Žalias mygtukas su tekstu ir ikona prekyje
-      5. Geltonas mygtukas TIK su ikona
-    */}
+    <Typography variant="h6">Tuščiaviduris didelis mygtukas:</Typography>
+    <Button size="large" variant="outlined">Button</Button>
+
+    <Typography variant="h6">Pilnaviduris mažas mygtukas:</Typography>
+    <Button size="small" variant="contained">Button</Button>
+
+    <Typography variant="h6">Raudonas mygtuka su tekstu ir  ikona gale:</Typography>
+    <Button color="error" variant="contained" endIcon={<AccessibilityIcon />}>Button</Button>
+
+    <Typography variant="h6">Žalias mygtukas su tekstu ir ikona prekyje:</Typography>
+    <Button color="success" variant="contained" startIcon={<AccessibilityIcon />}>Button</Button>
+
+    <Typography variant="h6">Geltonas mygtukas TIK su ikona:</Typography>
+    <IconButton>
+      <AccessibilityIcon />
+    </IconButton>
+
   </Container>
 );
 
