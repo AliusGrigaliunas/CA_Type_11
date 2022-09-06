65eed21a3059b0d0caacb2ac4ca1a90a83dff317
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-06 08:02:19 +0300

    init(88): creating forms and fields

---
+++ React [82-]/88 - Mui - creating forms and fields/src/app.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {
+  Typography,
+  Container,
+  Box,
+} from '@mui/material';
+import ButtonExample from './components/button-example';
+import CardExample from './components/card-example';
+
+const App: React.FC = () => (
+  <Container>
+    <Typography variant="h2" component="h1" align="center">Tai yra mano aplikacija</Typography>
+    <Box sx={{ display: 'flex', gap: 8, mt: 8 }}>
+      <ButtonExample />
+      <CardExample />
+    </Box>
+  </Container>
+);
+
+export default App;
