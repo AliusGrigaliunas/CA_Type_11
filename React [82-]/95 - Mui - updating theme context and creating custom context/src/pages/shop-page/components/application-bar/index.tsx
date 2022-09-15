d6c32581924f8ab0d3952bf1a693127ab14ac2c9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-15 11:05:13 +0300

    feat(95): context user for drawer layout manipulation

--- React [82-]/95 - Mui - updating theme context and creating custom context/src/pages/shop-page/components/application-bar/index.tsx
+++ React [82-]/95 - Mui - updating theme context and creating custom context/src/pages/shop-page/components/application-bar/index.tsx
@@ -6,35 +6,32 @@ import {
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ApplicationBarContainer from './application-bar-container';
+import DrawerContext from '../../contexts/drawer-context';
 
-type ApplicationBarProps = {
-  open: boolean,
-  openDrawer: VoidFunction,
+const ApplicationBar: React.FC = () => {
+  const { open, openDrawer } = React.useContext(DrawerContext);
+
+  return (
+    <ApplicationBarContainer position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={openDrawer}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(open && { display: 'none' }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Mini variant drawer
+        </Typography>
+      </Toolbar>
+    </ApplicationBarContainer>
+  );
 };
-
-const ApplicationBar: React.FC<ApplicationBarProps> = ({
-  open,
-  openDrawer,
-}) => (
-  <ApplicationBarContainer position="fixed" open={open}>
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={openDrawer}
-        edge="start"
-        sx={{
-          marginRight: 5,
-          ...(open && { display: 'none' }),
-        }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" noWrap component="div">
-        Mini variant drawer
-      </Typography>
-    </Toolbar>
-  </ApplicationBarContainer>
-);
 
 export default ApplicationBar;
