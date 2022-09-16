3cca18bd5657a858fccb8768488d7547676d3923
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-16 10:06:39 +0300

    feat(96): cup fetch logic exportted to cup-service

---
+++ React [82-]/96 - JSON server, fething data, useEffect/src/services/cup-service.ts
@@ -0,0 +1,17 @@
+import config from '../config';
+
+const { serverAddress } = config;
+const collectionName = 'cups';
+
+const fetchMany = async () => {
+  const response = await fetch(`${serverAddress}/${collectionName}`);
+  const fetchedCups = await response.json();
+
+  return fetchedCups;
+};
+
+const CupService = {
+  fetchMany,
+};
+
+export default CupService;
