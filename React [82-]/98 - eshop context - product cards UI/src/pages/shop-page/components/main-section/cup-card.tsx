740d732c21ee4c853aa01a9bb1f87fa5f0187c59
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-20 12:06:04 +0300

    feat(98): Product card UI implemented

--- React [82-]/98 - eshop context - product cards UI/src/pages/shop-page/components/main-section/cup-card.tsx
+++ React [82-]/98 - eshop context - product cards UI/src/pages/shop-page/components/main-section/cup-card.tsx
@@ -4,6 +4,7 @@ import {
   Paper,
   Box,
   Typography,
+  Button,
 } from '@mui/material';
 import Swiper from 'components/swiper';
 
@@ -17,41 +18,70 @@ const CupCard: React.FC<CupCardProps> = ({
   price,
 }) => (
   <Grid item xs={12} sm={6} md={4} lg={3}>
-    <Paper elevation={3} sx={{ height: 400 }}>
-      <Swiper images={images} />
-      <Box sx={{ p: 2, pt: 1 }}>
-        <Box sx={{ mb: 1 }}>
-          <Typography
-            component="div"
-            variant="h5"
-            sx={{
-              float: 'right',
-              ml: 1,
-              mb: 1,
-              color: 'success.main',
-              fontWeight: 'fontWeightMedium',
-            }}
-          >
-            {`${price}$`}
-          </Typography>
-          <Typography component="h2" variant="h5">{title}</Typography>
-        </Box>
-        <Box sx={{ height: 80 }}>
-          <Typography
-            sx={{
-              display: '-webkit-box',
-              WebkitLineClamp: 3,
-              WebkitBoxOrient: 'vertical',
-              overflow: 'hidden',
-              textOverflow: 'elipsis',
-            }}
-          >
-            {description}
-          </Typography>
+    <Paper
+      elevation={3}
+      sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}
+    >
+      <Box>
+        <Swiper images={images} />
+      </Box>
+      <Box sx={{
+        p: 2,
+        pt: 1,
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+      >
+        <Box>
+          <Box sx={{ mb: 1 }}>
+            <Typography
+              component="div"
+              variant="h5"
+              sx={{
+                float: 'right',
+                ml: 1,
+                mb: 1,
+                color: 'success.main',
+                fontWeight: 'fontWeightMedium',
+              }}
+            >
+              {`${price}$`}
+            </Typography>
+            <Typography component="h2" variant="h5">{title}</Typography>
+          </Box>
+          <Box sx={{ height: 80, my: 2 }}>
+            <Typography
+              sx={{
+                display: '-webkit-box',
+                WebkitLineClamp: 3,
+                WebkitBoxOrient: 'vertical',
+                overflow: 'hidden',
+                textOverflow: 'elipsis',
+              }}
+            >
+              {description}
+            </Typography>
+          </Box>
         </Box>
 
-        {id}
-        {price}
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button
+            variant="contained"
+            sx={{ width: '50%' }}
+            onClick={() => console.log('Išsaugoti:', id)}
+          >
+            Išsaugoti
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ width: '50%' }}
+            onClick={() => console.log('Dėti į krepšelį:', id)}
+          >
+            Dėti į krepšelį
+          </Button>
+        </Box>
       </Box>
     </Paper>
   </Grid>
@@ -60,21 +90,6 @@ const CupCard: React.FC<CupCardProps> = ({
 export default CupCard;
 
 /*
-  // 1. Iškelti Swiper komponentą į globalių komponentų aplanką.
-
-  // * 11:15
-  // 2. Po nuotrauką atvaizduokite Pavadinimą
-
-  // * 11:25
-  // 3. Išskirkite tekstui (description) ne daugiau nei 4 eilutes. PS. line-clamp
-
-  // * 11:35
-  // 4. Kaina atvaizduokite po nuotrauka dešinėje pusėje, taip kad netrūkdytų pavadinimui
-
-  * 11:50
-  5. Sukurkite 2 mygtukus, vienas šalia kito
-    * Peržiūrėti - kurį paspaudut atspausdinamas produkto id
-    * Dėti į krepšelį - kursį paspaudus atspausdinamas produkto id
 
   6. pritaikykite koretelės reikiamą aukštį naudojant:
     * https://smartdevpreneur.com/5-mui-sx-breakpoint-examples/
