dcab896f3810a56b0a0ff4c22a528cc199dbb999
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-20 11:03:12 +0300

    task-done(98): swiper exported to sparate component

--- React [82-]/98 - eshop context - product cards UI/src/pages/shop-page/components/main-section/cup-card.tsx
+++ React [82-]/98 - eshop context - product cards UI/src/pages/shop-page/components/main-section/cup-card.tsx
@@ -3,20 +3,10 @@ import {
   Grid,
   Paper,
   Box,
-  styled,
 } from '@mui/material';
-import { Swiper as SwiperJS, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import Img from 'components/img';
+import Swiper from 'components/swiper';
 
 type CupCardProps = Omit<Cup, 'categoryId' | 'materialTypeId' | 'colorId'>;
-
-const Swiper = styled(SwiperJS)({
-  height: 250,
-});
 
 const CupCard: React.FC<CupCardProps> = ({
   id,
@@ -27,19 +17,7 @@ const CupCard: React.FC<CupCardProps> = ({
 }) => (
   <Grid item xs={12} sm={6} md={4} lg={3}>
     <Paper elevation={3} sx={{ height: 400 }}>
-      <Swiper
-        pagination={{
-          dynamicBullets: true,
-        }}
-        navigation
-        modules={[Pagination, Navigation]}
-      >
-        {images.map((imgSrc) => (
-          <SwiperSlide key={imgSrc}>
-            <Img src={imgSrc} sx={{ height: '100%', width: '100%' }} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <Swiper images={images} />
       <Box sx={{ p: 3 }}>
         {id}
         {title}
@@ -53,18 +31,23 @@ const CupCard: React.FC<CupCardProps> = ({
 export default CupCard;
 
 /*
-  1. Iškelti Swiper komponentą į globalių komponentų aplanką
+  // 1. Iškelti Swiper komponentą į globalių komponentų aplanką.
 
+  * 11:15
   2. Po nuotrauką atvaizduokite Pavadinimą
 
+  * 11:25
   3. Išskirkite tekstui (description) ne daugiau nei 4 eilutes. PS. line-clamp
 
+  * 11:35
   4. Kaina atvaizduokite po nuotrauke dešinėje pusėje, taip kad netrūkdytų pavadinimui
 
+  * 11:45
   5. Sukurkite 2 mygtukus, vienas šalia kito
     * Peržiūrėti - kurį paspaudut atspausdinamas produkto id
     * Dėti į krepšelį - kursį paspaudus atspausdinamas produkto id
-
+    *
+  * 11:55
   6. pritaikykite koretelės reikiamą aukštį naudojant:
     * https://smartdevpreneur.com/5-mui-sx-breakpoint-examples/
         * skiltis - Create Responsive Layout with MUI SX Breakpoints
