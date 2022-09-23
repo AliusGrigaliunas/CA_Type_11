f63524877228e22fb535e43c66760ab9b4854ee9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-23 09:55:50 +0300

    feat(101): useCheckboxFilter urlParam sync with selectOptions on first load

--- React [82-]/101 - eshop context - data fetching based on queryParams/src/pages/shop-page/hooks/use-checkbox-filter.tsx
+++ React [82-]/101 - eshop context - data fetching based on queryParams/src/pages/shop-page/hooks/use-checkbox-filter.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { CheckboxOption } from 'components/form-controls/checkbox-group';
 import { useSearchParams } from 'react-router-dom';
+import useMounted from 'hooks/use-mounted';
 
 type CheckboxFilterOptions = {
   urlParamName?: string
@@ -13,26 +14,43 @@ type UseCheckboxFilter = (props: CheckboxFilterOptions) => [
   CheckboxOption[],
 ];
 
+const urlValuesNotInOptions = (values: string[], options: CheckboxOption[]) => {
+  const optionsValues = options.map(({ value }) => value);
+
+  return JSON.stringify(values) !== JSON.stringify(optionsValues);
+};
+
 const useCheckboxFilter: UseCheckboxFilter = ({ urlParamName, fetchOptions }) => {
+  const isMounted = useMounted();
   const [options, setOptions] = React.useState<CheckboxOption[]>([]);
   const [selectedOptions, setSelectedOptions] = React.useState<CheckboxOption[]>([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const initializeOptions = async () => {
-    const fetchedOptions = await fetchOptions();
-    setOptions(fetchedOptions);
-  };
-
   React.useEffect(() => {
-    initializeOptions();
+    (async () => {
+      const fetchedOptions = await fetchOptions();
+
+      setOptions(fetchedOptions);
+      if (urlParamName) {
+        const urlValues = searchParams.getAll(urlParamName);
+        const urlOptions = fetchedOptions.filter(({ value }) => urlValues.includes(value));
+
+        setSelectedOptions(urlOptions);
+      }
+    })();
   }, []);
 
   React.useEffect(() => {
-    if (urlParamName !== undefined) {
-      searchParams.delete(urlParamName);
-      selectedOptions.forEach(({ value }) => searchParams.append(urlParamName, value));
+    if (urlParamName !== undefined && isMounted) {
+      const urlValues = searchParams.getAll(urlParamName);
+      const shouldUpdate = urlValuesNotInOptions(urlValues, selectedOptions);
 
-      setSearchParams(searchParams);
+      if (shouldUpdate) {
+        searchParams.delete(urlParamName);
+        selectedOptions.forEach(({ value }) => searchParams.append(urlParamName, value));
+
+        setSearchParams(searchParams);
+      }
     }
   }, urlParamName !== undefined ? [selectedOptions] : []);
 
