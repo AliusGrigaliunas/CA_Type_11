5e8fbe5e1f1158f05bddbb09ef48063b34a48f81
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-26 08:04:42 +0300

    init(102): react router dom

---
+++ React [82-]/102 - react router - links, layouts, dynamic pages/src/pages/shop-page/components/application-bar/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Toolbar,
+  Typography,
+  AppBar,
+} from '@mui/material';
+
+const ApplicationBar: React.FC = () => (
+  <AppBar position="fixed">
+    <Toolbar>
+      <Typography variant="h6" noWrap component="div">
+        Mini variant drawer
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+export default ApplicationBar;
