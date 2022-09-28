99ea9f6a53782ddd09baa2e15ede70cd31e3ed37
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-09-28 11:41:50 +0300

    task(104): 1'st part

--- React [82-]/104 - formik - controlled form fields and validation/src/pages/auth/register-page.tsx
+++ React [82-]/104 - formik - controlled form fields and validation/src/pages/auth/register-page.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { TextField, Autocomplete } from '@mui/material';
 import CheckboxGroup, { CheckboxOption } from 'components/form-controls/checkbox-group';
+import CategoriesService from 'services/categories-service';
+import MarketingIntrestsService from 'services/marketing-intrests-service';
 import AuthForm from './components/auth-form';
 
 /*
   1. Parsiųsti ir būsenos kintamuosiuose išsaugoti categories ir marketingIntrests duomenis
-    * categories duomenis pritaikykite ir pateikite Autocomplete field'ui 
-    * marketingIntrests duomenis pritaikykite ir pateikite CheckboxGroup field'ui 
+    * categories duomenis pritaikykite ir pateikite Autocomplete field'ui
+    * marketingIntrests duomenis pritaikykite ir pateikite CheckboxGroup field'ui
   2. Sukurti Formik helper'ius naudojant useFormik hook'są
     * Įrašykite pradines reikšmes:
     * Įrašykite onSubmit funkciją
@@ -15,22 +17,48 @@ import AuthForm from './components/auth-form';
     * paliestumo būsenos saugojimą formik.fields objekte
     * pateikite validacijos schemą (KOLKAS TIK TextField LAUKAMS)
     * įgalinkite klaidų rodymą
-  4. Pritaikykite AuthForm komponentui validumo reikšmė naudojant formik.dirty ir formik.isValid helper'ius 
+  4. Pritaikykite AuthForm komponentui validumo reikšmė naudojant formik.dirty ir formik.isValid
+  helper'ius
 */
+
+const categoryToCheckboxOption = ({ id, title }: Category): CheckboxOption => ({
+  value: id,
+  label: title,
+});
+
+const marketingIntrestToCheckboxOption = ({ id, title }: MarketingIntrest): CheckboxOption => ({
+  value: id,
+  label: title,
+});
+
 const RegisterPage: React.FC = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [categoriesOptions, setCategoriesOptions] = React.useState<CheckboxOption[]>([]);
+  const [
+    matketingIntrestsOptions,
+    setMarketingIntrestsOptions,
+  ] = React.useState<CheckboxOption[]>([]);
 
+  React.useEffect(() => {
+    (async () => {
+      const [
+        fetchedCategories,
+        fetchedMatketingIntrests,
+      ] = await Promise.all([
+        CategoriesService.fetchMany(),
+        MarketingIntrestsService.fetchMany(),
+      ]);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    console.log('Siunčiami duomenys į serverį, naudojant globalios būsenos valdymo įrankį:');
-  };
+      setCategoriesOptions(fetchedCategories.map(categoryToCheckboxOption));
+      setMarketingIntrestsOptions(fetchedMatketingIntrests.map(marketingIntrestToCheckboxOption));
+    })();
+  }, []);
 
   return (
-    <AuthForm title="Registracija" submitText="Registruotis" onSubmit={handleSubmit}>
+    <AuthForm title="Registracija" submitText="Registruotis" onSubmit={() => { }}>
       <TextField
+        name="email"
         variant="filled"
         label="El. paštas"
         type="email"
@@ -38,6 +66,7 @@ const RegisterPage: React.FC = () => {
         onChange={(e) => setEmail(e.target.value)}
       />
       <TextField
+        name="password"
         variant="filled"
         label="Slaptažodis"
         type="password"
@@ -45,21 +74,21 @@ const RegisterPage: React.FC = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <Autocomplete
-        options={...}
+        options={categoriesOptions}
         multiple
-        getOptionLabel={...}
         renderInput={(inputProps) => (
           <TextField
-            label="..."
+            label="Dominančios kategorijos"
+            name="categories"
             variant="filled"
             {...inputProps}
           />
         )}
       />
       <CheckboxGroup
-        label="..."
-        name="..."
-        options={...}
+        label="Dominančios reklamos"
+        name="marketingIntrests"
+        options={matketingIntrestsOptions}
       />
     </AuthForm>
   );
